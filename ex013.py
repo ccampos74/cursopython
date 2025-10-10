b08ddd0f7beaa432f6b0f361f9e87e4ddfbbe5c7
Claudio_Campos <chpc6955@gmail.com>
2025-10-10 15:19:06 -0300

    update ex013.py
    comentário de cálculo simplificado

--- ex013.py
+++ ex013.py
@@ -1,4 +1,5 @@
-sal = float(input('Digite o salário: '))
+sal = float(input('Digite o salário: R$'))
 aum = (sal / 100) * 15
 salatual = sal + aum
 print('O salário de R${:.2f} com o aumento de 15%, passa a valer R${:.2f}'.format(sal, salatual))
+#sal + (sal * 15 / 100)
