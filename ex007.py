3d44026272105dd702370ec21976811a8f0a85e4
Claudio_Campos <chpc6955@gmail.com>
2025-10-10 14:03:32 -0300

    update ex007.py

--- ex007.py
+++ ex007.py
@@ -1,4 +1,4 @@
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 media = (nota1 + nota2) / 2
-print('A média de {} e {}, é igual a: {}'.format(nota1, nota2, media))
+print('A média de {:.1f} e {:.1f}, é igual a: {:.1f}'.format(nota1, nota2, media))
