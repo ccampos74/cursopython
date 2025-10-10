a6ccd7ef30282cdbaa1bb1b933678e8b9280df53
Claudio_Campos <chpc6955@gmail.com>
2025-10-10 15:15:13 -0300

    update ex012.py
    Acrescentando comentário de forma de cálculo mais fácil

--- ex012.py
+++ ex012.py
@@ -1,4 +1,5 @@
-preco = float(input('Digite o preço do produto: '))
+preco = float(input('Digite o preço do produto: R$'))
 desc = (preco / 100) * 5
 novopreco = preco - desc
 print('O produto que custava R${:.2f}, com o desconto de 5% passa a custar R${:.2f}'.format(preco, novopreco))
+#preco - (preco * 5 / 100)
