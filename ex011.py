fb0dd9bd83d9e0c3675fbc6abb3637362a82a0f2
Claudio_Campos <chpc6955@gmail.com>
2025-10-10 15:04:38 -0300

    update ex012.py

--- ex011.py
+++ ex011.py
@@ -3,4 +3,4 @@ altura = float(input('Digite a altura da parede: '))
 parede = largura * altura
 tinta = parede / 2
 print('Uma área com {} metros de largura e {} metros de altura, é igual a {}m²'.format(largura, altura, parede))
-print('Para pintar a área, será gosto {} litro(s) de tinta.'.format(tinta))
+print('Para pintar a área, será gasto {} litro(s) de tinta.'.format(tinta))
