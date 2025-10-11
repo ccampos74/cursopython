5c7c4aee48a8e5426dd68f97a66e7bd1d562596e
Claudio_Campos <chpc6955@gmail.com>
2025-10-11 12:02:45 -0300

    update ex021.py
    Após trocar a pasta parou de funcionar, solução: caminho completo até o arquivo

--- mundo-01/exercicios/ex021.py
+++ mundo-01/exercicios/ex021.py
@@ -1,6 +1,6 @@
 import pygame
 pygame.init()
-pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.load('/home/claudio/Estudos/cursopython/mundo-01/exercicios/ex021.mp3')
 pygame.mixer.music.play()
 input()
 pygame.event.wait()
