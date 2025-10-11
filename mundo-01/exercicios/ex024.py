59a085efa5346d237cccb288127e48280047572a
Claudio_Campos <chpc6955@gmail.com>
2025-10-11 16:22:41 -0300

    Create ex024.py

---
+++ mundo-01/exercicios/ex024.py
@@ -0,0 +1,6 @@
+cid = str(input('Em que cidade você nasceu? ')).strip()
+cidade = cid.split()[0].upper()
+print('SANTO' in cidade)
+
+# cid = str(input('Em que cidade você nasceu? ')).strip()
+# print(cid[:5].upper() == 'SANTO')
