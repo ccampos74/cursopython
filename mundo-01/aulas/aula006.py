7947e60f8aa218d4468e3b0ac89b1c95327d20d0
Claudio_Campos <chpc6955@gmail.com>
2025-10-10 23:42:55 -0300

    Create aula006.py

---
+++ mundo-01/aulas/aula006.py
@@ -0,0 +1,14 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite mais um número: '))
+s = n1 + n2
+print('A soma vale', s)
+#outra forma de print
+print('A soma vale {}'.format(s))
+
+ 
+'''int: 7 -4 0 9875
+   float: 4.5 0.076 -15.223 7.0
+   bool: True False
+   str: 'Olá' '7.5' ' '
+'''
+
