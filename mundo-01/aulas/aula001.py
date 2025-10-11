1d0eee630004869216d3d5c299511b7f6c502424
Claudio_Campos <chpc6955@gmail.com>
2025-10-10 23:18:24 -0300

    Create aula001.py

---
+++ mundo-01/aulas/aula001.py
@@ -0,0 +1,11 @@
+print('Olá, Mundo!')
+print(7+4)
+print('7'+'4')
+nome = 'Guanabara'
+idade = 25
+peso = 75.8
+print(nome, idade, peso)
+nome1= input('Qual é o seu nome? ')
+idade1 = input('Qual é a sua idade? ')
+peso1 = input('Qual é o seu peso? ')
+print(nome1, idade1, peso1)
