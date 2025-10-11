ada50de06033722fabd3707ef54f5862b98c0890
Claudio_Campos <chpc6955@gmail.com>
2025-10-11 18:47:52 -0300

    Create ex027.py
    Primeiro e último nome separadamente

---
+++ mundo-01/exercicios/ex027.py
@@ -0,0 +1,10 @@
+nome = str(input('Digite seu nome completo: ')).strip().title()
+print('Muito prazer em te conhecer!')
+print('Seu primeiro nome é {}'.format(nome.split()[0]))
+print('Seu último nome é {}'.format(nome.split()[-1]))
+ 
+# n = str(input('Digite seu nome completo: ')).strip()
+# nome = n.split()
+# print('Muito prazer em te conhecer!')
+# print('Seu primeiro nome é {}'.format(nome[0]))
+# print('Seu último nome é {}'.format(nome[len(nome)-1]))
