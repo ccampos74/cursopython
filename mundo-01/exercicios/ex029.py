fb308494e2b3d98a5557526f9fc1742738428058
Claudio_Campos <chpc6955@gmail.com>
2025-10-12 13:21:48 -0300

    update ex029.py

--- mundo-01/exercicios/ex029.py
+++ mundo-01/exercicios/ex029.py
@@ -1,12 +1,16 @@
 from random import randint
+from time import sleep
 computador = randint(0, 5) # faz o computador "PENSAR"
-print('-=-'*20)
-print("Vou pensar em um número de 0 a 5, tente adivinhar.")
-print('-=-'*20)
+print('\033[34m-=-\033[m'*20)
+print("\033[33mVou pensar em um número de 0 a 5, tente adivinhar.\033[m")
+print('\033[34m-=-\033[m'*20)
 usuario = int(input('Digite seu palpite: '))
+print('PROCESSANDO...')
+sleep(3)
 print('Computador escolheu {}'.format(computador))
 print('Você escolheu {}'.format(usuario))
 if usuario == computador:
     print('Parabéns! Você venceu!')
 else:
     print('Você perdeu! Tente novamente.')
+    
