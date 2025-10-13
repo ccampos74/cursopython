3231fc50afba5500a6439c57feac791d25a2e064
Claudio_Campos <chpc6955@gmail.com>
2025-10-13 20:54:19 -0300

    Update ex045.py

--- mundo-02/exercicios/ex045.py
+++ mundo-02/exercicios/ex045.py
@@ -1,47 +1,40 @@
-print('{}JO{}{}KEN{}{}PÔ{}'.format('\033[31m', '\033[m', '\033[33m', '\033[m', '\033[34m', '\033[m'))
 from random import randint
 from time import sleep
-itens = ('PEDRA', 'PAPEL', 'TESOURA')
-comp = randint(0, 2)
-print('''Suas opções:
+print('{}JO{}KEN{}PÔ{} 2.0{}'.format('\033[32m', '\033[33m', '\033[34m', '\033[31m', '\033[m'))
+print('''OPÇÕES:
 [0] PEDRA
 [1] PAPEL
 [2] TESOURA''')
-usr = int(input('Qual é a sua jogada? ')) 
-sleep(1) 
-print('{}JO{}'.format('\033[31m', '\033[m'))
+usr = int(input('Digite sua opção: '))
+comp = randint(0, 2)
+itens = ('PEDRA', 'PAPEL', 'TESOURA')
+print('{}JO{}'.format('\033[32m', '\033[m'))
 sleep(1)
 print('{}KEN{}'.format('\033[33m', '\033[m'))
 sleep(1)
 print('{}PÔ!!!{}'.format('\033[34m', '\033[m'))
-print('-='*15)
-print('O computador jogou {}'.format(itens[comp]))
-print('O usuário jogou {}'.format(itens[usr]))
-print('-='*15)
-if comp == 0: #comp jogou pedra
-    if usr == 0:
-        print('EMPATE')
+sleep(1)
+if usr <= 2:
+    print('-='*12)
+    print('Usuário jogou {}'.format(itens[usr]))
+    print('Computador jogou {}'.format(itens[comp]))
+    print('-='*12)
+    if usr == comp:
+        print('{}EMPATE{}'.format('\033[33m', '\033[m'))
+    elif usr == 0:
+        if comp == 1:
+            print('{}COMPUTADOR VENCEU!!!{}'.format('\033[36m', '\033[m'))
+        elif comp == 2:
+            print('{}USUÁRIO VENCEU!!!{}'.format('\033[32m', '\033[m'))
     elif usr == 1:
-        print('JOGADOR VENCEU!!!')
+        if comp == 0:
+            print('{}USUÁRIO VENCEU!!!{}'.format('\033[32m', '\033[m'))
+        elif comp == 2:
+            print('{}COMPUTADOR VENCEU!!!{}'.format('\033[36m', '\033[m'))
     elif usr == 2:
-        print('COMPUTADOR VENCEU!!!')
-    elif usr > 2:
-        print('OPÇÃO INVÁLIDA')
-elif comp == 1: #comp jogou papel
-    if usr == 0:
-        print('COMPUTADOR VENCEU!!!')
-    elif usr == 1:
-        print('EMPATE')
-    elif usr == 2:
-        print('JOGADOR VENCEU!!!')
-    elif usr > 2:
-        print('OPÇÃO INVÁLIDA')
-elif comp == 2: #comp jogou tesoura
-    if usr == 0:
-        print('JOGADOR VENCEU!!!')
-    elif usr == 1:
-        print('COMPUTADOR VENCEU!!!')
-    elif usr == 2:
-        print('EMPATE')
-    elif usr > 2:
-        print('OPÇÃO INVÁLIDA')
+        if comp == 0:
+            print('{}COMPUTADOR VENCEU!!!{}'.format('\033[36m', '\033[m'))
+        elif comp == 1:
+            print('{}USUÁRIO VENCEU!!!{}'.format('\033[32m', '\033[m'))
+else:
+    print('{}OPÇÃO INVÁLIDA{}'.format('\033[31m', '\033[m'))
