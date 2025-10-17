d2c1e200d89f1f632dd6409387e25e0df9c1198a
Claudio_Campos <chpc6955@gmail.com>
2025-10-17 17:13:47 -0300

    Create ex064.py

---
+++ mundo-02/exercicios/ex064.py
@@ -0,0 +1,10 @@
+num = 0
+cont = 0
+soma = 0
+#num = cont = soma = 0
+num = int(input('Digite um número [999 para parar]: '))
+while num != 999:
+    soma += num
+    cont += 1
+    num = int(input('Digite um número [999 para parar]: '))
+print('Você digitou {} números e a soma é {}.'.format(cont, soma))
